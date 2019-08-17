2c603772e867379d21bee3141b339593118db4e1
CharchitVar <charchit.v05@gmail.com>
2019-08-17 13:05:13 +0530

    Init Commit

---
+++ src/app.js
@@ -0,0 +1,98 @@
+const path=require('path');
+const express =require('express');
+const app=express();
+const hbs=require('hbs'); //For Partials these are the part of the view which cpuld be common for every page like footer or headers
+const weather=require('./utils/weatherResult');
+const geoCode=require('./utils/geoCode');
+
+//Define path for express config
+const publicDirectorypath=path.join(__dirname,'../public');
+const viewPath=path.join(__dirname,'../templates/views');
+const partialsPath=path.join(__dirname,'../templates/partials');     
+hbs.registerPartials(partialsPath);
+
+//setup handlerbars engines and views location
+app.set('view engine','hbs');
+app.set('views',viewPath);
+
+
+//setup static directory to serve
+app.use(express.static(publicDirectorypath));
+
+
+
+//------main code starts from here
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:'Weather App',
+        name:'Charchit'
+    })
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title:'About me',
+        name:'Charchit',
+        location:'Aligarh'
+    })
+})
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        title:'Help',
+        helptext:'Provide an Help message here buddy !!!!',
+        name:'Charchit'
+        
+    })
+})
+
+app.get('/help/*',(req,res)=>{
+    res.render('404',{
+        title:'Weather',
+        errorMessage:'404 Help Page Not Found'
+
+    })
+})
+app.get('/weather',(req,res)=>{
+    if(!req.query.address){
+        return res.send({
+            error:'You must Provide a Addres '
+        })
+    }
+    geoCode(req.query.address,(error,geoCodeResult)=>{
+        if(error){
+             res.send({
+                error:error
+            })
+        }
+        else if(geoCodeResult){
+        weather(geoCodeResult.latitude,geoCodeResult.longitude,(error,weatherResult)=>{
+            if(error){
+                res.send({
+                    error:error
+                })
+            }
+            else if(weatherResult){
+                res.send({
+                    WeatherResult:`The Weather Result of ${geoCodeResult.location} is ${weatherResult}`
+                })
+              
+            }
+        })
+            }
+        }
+        )
+})
+
+
+app.get('*',(req,res)=>{
+    res.render('404',{
+        title:'Weather',
+        errorMessage:'404 Page Not Found'
+
+    })
+})
+
+
+
+
+app.listen(4500);
---
+++ src/utils/weatherResult.js
@@ -0,0 +1,31 @@
+const request=require('request');
+
+const weather=(latitude,longitude,callback)=>{
+    const url="https://api.darksky.net/forecast/58c8b3e23a86d14b0492572c6f5fd921/"+encodeURIComponent(latitude)+","+encodeURIComponent(longitude)+"?lang=en&units=si"
+    request({url:url,json:true},(error,response)=>{
+    //const data=JSON.parse(response.body);
+
+    if(error){
+        callback("Unable to Get the Weather",undefined);
+        //console.log(); //If response does not come means server does not get connected
+    }
+    else if(response.body.error){
+        callback("Error Message from the server"+response.body.error,undefined)
+ // error recieved if Wrong /Faulty Inpput Given
+    }
+    else{ // If proper Recieved
+    const data=response.body;   
+    const location=data.timezone;
+    const weekSummary=data.daily.data[0].summary;
+    const currentWeather=data.currently.temperature;
+    const precipitation=data.currently.precipProbability;
+    const weatherResult=` ${weekSummary} The Current Temperature  is
+    ${currentWeather}. There will be ${precipitation} Rain Chances`
+    callback(undefined,weatherResult);
+    
+    }
+}
+)
+}
+
+module.exports=weather;
